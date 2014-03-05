3f3674a78601e5ad4b52db4ff9666ab22767f7c1
相澤 隆志 <aizawakeisuke@aisawa-no-MacBook-Air.local>
2014-03-05 11:34:26 +0900

    Initial Commit

---
+++ AddTabtest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AddTabtest
+//
+//  Created by 相澤 隆志 on 2014/03/05.
+//  Copyright (c) 2014年 相澤 隆志. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
